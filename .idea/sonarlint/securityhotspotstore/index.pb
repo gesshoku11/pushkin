e8952628ce8590635c88101346a703f8e58fa1a9
Ilya Saramud <ilyasaramud11@gmail.com>
2024-12-02 02:11:41 +0300

    Добавлен обработчик сообщений для Telegram-бота

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -3,3 +3,7 @@
 main.py,f\1\f1bdda93d9a278e358509d498e17d97764c1fb29
 >
 credentials.py,7\c\7cc75fcb1c9eabb2c6e0e4838a89f723f8cd764b
+6
+bot.py,9\3\93374d5ad20eb17306a1988725b9b8f5bbaeaf0e
+6
+gpt.py,0\2\02acc706ae88bb4481efeaf0c9e032ad6c651b74
--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from telegram import Update
+from telegram.ext import Application, MessageHandler, filters, ContextTypes
 
+# Функция для обработки сообщений
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+    print(text)
 
-token = '7714966043:AAHoLDGuylx7uClhr6FcJ_AIWVjuvTzve2M'
+token = '7714966043:AAHoLDGuylx7uClhr6FcJ_AIWVjuvTzve2M' # TODO Убрать из кода !
+
+app = Application.builder().token(token).build()
+app.add_handler(MessageHandler(filters.TEXT, start))
+
+app.run_polling()
